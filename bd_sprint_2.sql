d446874db58d9b0cb45e30d9de7ae58758dfea24
Marques <luiza.b.marques@accenture.com>
2022-04-25 00:49:15 -0300

    alteraco script sprint 2

--- bd_sprint_2.sql
+++ bd_sprint_2.sql
@@ -2,11 +2,11 @@ CREATE DATABASE algas_2;
 USE algas_2;
 
 CREATE TABLE  dados (
-    _id_ int  primary key auto_increment,
-    idade int ,            
-    genero  varchar(10),      
-    regiao  varchar(20), 
-    idade_descoberta int,
-    tempo decimal(12,9),                  
-    memoria decimal(12,5)
+    '_id_' int  primary key auto_increment,
+    'idade' int ,            
+    'genero' varchar(10),      
+    'regiao' varchar(20), 
+    'idade_descoberta' int,
+    'tempo' decimal(12,9),                  
+    'memoria' decimal(12,5)
 );
