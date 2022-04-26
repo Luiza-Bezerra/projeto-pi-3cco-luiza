5189c3bffb67bc8982c2733e5972f577f79b0f64
Marques <luiza.b.marques@accenture.com>
2022-04-26 19:35:21 -0300

    ajuste script de geração

--- generate_sprint_2.py
+++ generate_sprint_2.py
@@ -8,17 +8,17 @@ def geracao(entradas):
     data_list = []
 
     for i in range(1, entradas):
-        idade = random.randint(17,64)
-        idade_descoberta = random.randint(17,idade)
+        idade_atual = random.randint(17,64) # Foco jovens e adultos com autismo
+        idade_descoberta = random.randint(0,idade_atual) 
 
         fim = time.time()
         memoria = round(mp.memory_usage()[0],2)
 
         data_list.append({
-            'idade': idade, # Idade da pessoa altista
-            'genero': random.choice(['M','F']), # F-feminino, M-Masculino
+            'idade_atual': idade_atual, # Idade da pessoa autista
+            'genero': random.choice(['M','M','M','M','M','F']), # F-feminino | M-Masculino | (1 F a cada 5 M possui autismo)
             'regiao': random.choice(['S','N','SE','NE','CO']), # Regiões do Brasil
-            'idade_descoberta': idade_descoberta, # Idade que descobriou o altismo
+            'idade_descoberta': idade_descoberta, # Idade que descobriou o autismo
             'tempo': (fim - inicio), # Tempo de execução
             'memoria': memoria # Memória usada na execução
         })
@@ -38,15 +38,15 @@ def salvar(lista_dados):
     try:
         connection = get_conn()
 
-        idade = int(lista_dados.get('idade',None))
+        idade_atual = int(lista_dados.get('idade_atual',None))
         genero = str(lista_dados.get('genero',None))
         regiao = str(lista_dados.get('regiao',None))
-        descoberta = int(lista_dados.get('idade_descoberta',None))
+        idade_descoberta = int(lista_dados.get('idade_descoberta',None))
         tempo = round(float(lista_dados.get('tempo', None)), 9)
         memoria = round(float(lista_dados.get('memoria',None)), 5)
 
 
-        query = f"INSERT INTO dados(idade, genero, regiao, descoberta, tempo, memoria) VALUES({idade},'{genero}','{regiao}',{descoberta},{tempo},{memoria});"
+        query = f"INSERT INTO dados(idade, genero, regiao, descoberta, tempo, memoria) VALUES({idade_atual},'{genero}','{regiao}',{idade_descoberta},{tempo},{memoria});"
         print(f'Query:{query}\n')
 
         cursor = connection.cursor()
@@ -60,6 +60,6 @@ def salvar(lista_dados):
 if __name__ == "__main__":
     for i in range(5,20,1):
         data_list = geracao(i)
-        print(f'Dados gerados:\n{data_list}\n')
+
         for data in data_list:
             salvar(data)
