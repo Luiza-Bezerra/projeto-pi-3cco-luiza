b15a5a3aa4220e18542afdb218afdab20342fcf2
Marques <luiza.b.marques@accenture.com>
2022-04-26 21:06:49 -0300

    ajuste script de geração e readme

--- bd_sprint_2.sql
+++ bd_sprint_2.sql
@@ -2,11 +2,11 @@ CREATE DATABASE algas_2;
 USE algas_2;
 
 CREATE TABLE  dados (
-    _id_ int  primary key auto_increment,
-    idade int ,            
-    genero varchar(10),      
-    regiao varchar(20), 
-    descoberta int,
-    tempo decimal(12,9),                  
+    _id_ int  primary key auto_increment, -- identificador
+    idade int , -- idade atual da pessoa
+    genero varchar(10), -- genero da pessoa
+    regiao varchar(20), -- regiao onde a pessoa mora
+    descoberta int, -- idade que foi diagnosticada
+    tempo decimal(12,9),  
     memoria decimal(12,5)
 );
