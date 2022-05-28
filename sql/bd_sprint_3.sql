f3195fd763a788e6e3c4e6ef81c53428c505ba9c
Marques <luiza.b.marques@accenture.com>
2022-05-28 00:57:06 -0300

    add script banco sprint 3 azure

---
+++ sql/bd_sprint_3.sql
@@ -0,0 +1,52 @@
+-- CREATE DATABASE   autism_data  ;
+-- USE   autism_data  ; Grupo092022
+
+CREATE TABLE    paciente   (
+    _id_   int  primary key identity, 
+    idade_atual   int, 
+    idade_descoberta   int,
+    genero   bit, 
+    grau   varchar(10),
+    sensibilidade_sentidos  bit,
+    agressivo  bit,
+    hiperativo  bit,
+    movimentos_repetitivos  bit,
+    baixa_concentracao  bit,
+    hiperfoco  bit,
+    necessidade_rotina  bit,
+    dificuldade_imaginacao  bit,
+    introvertido  bit,
+    tipo_autismo   varchar(50),
+    tipo_autismo_descricao   varchar(max),
+);
+
+
+CREATE TABLE   localidade   (
+    _id_   int  primary key identity,
+    _FK_paciente   int ,
+    pais   varchar(20),
+    PIB_nacional  bigint,
+    regiao    varchar(20),
+    PIB_percent_BR   bigint,
+    PIB_regional   bigint,
+    populacao_regiao   bigint,
+    densidade_demografica_regiao  decimal(12,2) ,
+    area_regiao   bigint,
+    regiao_geoeconomica  bigint,
+    gentilico_regiao   varchar(20),
+    raca_predominante_regiao  varchar(20),
+    FOREIGN KEY (  _FK_paciente  ) REFERENCES   paciente  (  _id_  )
+);
+
+
+CREATE TABLE   renda   (
+    _id_   int  primary key identity,
+	_FK_paciente   int ,
+    classe  varchar(20),
+    classe_descricao  varchar(40),
+    quantidade_familia  int,
+    total_salario   decimal(12,2),
+    empregado  bit,
+    per_capita  decimal(12,2)
+    FOREIGN KEY (  _FK_paciente  ) REFERENCES   paciente  (  _id_  )
+);
